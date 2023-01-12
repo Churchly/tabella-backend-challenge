64194a3c37dd4f1538947dde32e940fbf8609eba
Lucas de Morais M Tavares <lucasmoraismt@gmail.com>
2023-01-11 23:12:09 -0300

    fix: removing duplicated file

--- src/database/config/index.ts
+++ src/database/config/index.ts
@@ -1,12 +1,13 @@
 // import "reflect-metadata"
 // import { DataSource } from "typeorm"
+// import { Email, Submission } from "../entities"
 
 // export const AppDataSource = new DataSource({
 //   type: "mongodb",
-//   database: "test",
+//   database: "churchly",
 //   synchronize: true,
 //   logging: false,
-//   entities: [],
-//   migrations: ["src/migration/**/*.ts"],
+//   entities: [Email, Submission],
+//   migrations: ["../migrations/**/*.ts"],
 //   subscribers: [],
 // })
